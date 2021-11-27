922675a17f20a16c1684f5e7316eb597ab29b180
Alfaa719 <nurul.hidayat.f3@gmail.com>
2021-11-27 16:53:46 +0700

    Finish CRUD Mahasiswa

--- controllers/Mahasiswa.js
+++ controllers/Mahasiswa.js
@@ -8,5 +8,47 @@ const getAllMahasiswa = async (req, res) => {
         res.json({message: error.message})
     }
 }
+const createMahasiswa = async (req, res) => {
+    try {
+        await Mahasiswa.create(req.body);
+        res.json({
+            'message': 'Mahasiswa created'
+        });
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
+const editMahasiswa = async (req, res) => {
+    try {
+        await Mahasiswa.update(req.body, {
+            where: { id: req.params.id }
+        })
+        res.json({
+            'message': 'Mahasiswa updated'
+        });
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
+const getMahasiswaById = async (req, res) => {
+    try {
+        const mahasiswa = await Mahasiswa.findOne({
+            where: {id: req.params.id}
+        })
+        res.json(mahasiswa);
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
+const deleteMahasiswa = async (req, res) => {
+    try {
+        await Mahasiswa.destroy({where: {id: req.params.id}})
+        res.json({
+            'message': 'Mahasiswa deleted'
+        });
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
 
-module.exports = { getAllMahasiswa };
+module.exports = { getAllMahasiswa, createMahasiswa, editMahasiswa, getMahasiswaById, deleteMahasiswa };
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { getAllMahasiswa } = require('../controllers/mahasiswa');
+const { getAllMahasiswa, createMahasiswa, editMahasiswa, getMahasiswaById, deleteMahasiswa } = require('../controllers/mahasiswa');
 
 const router = express.Router();
 
 // Mahasiswa route
 router.get('/', getAllMahasiswa);
+router.post('/', createMahasiswa);
+router.patch('/:id', editMahasiswa);
+router.get('/:id', getMahasiswaById);
+router.delete('/:id', deleteMahasiswa);
 
 module.exports = router;
