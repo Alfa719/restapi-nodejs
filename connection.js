24473d9c7220c8c1d974ac4366168d1e0a2f9020
Alfaa719 <nurul.hidayat.f3@gmail.com>
2021-11-27 13:14:05 +0700

    Create MySQL connection in connection.js

---
+++ connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+// Buat koneksi
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dbrestapi'
+});
+connection.connect((err) => {
+    if (err) throw console.error('Connection fail : ' + err);;
+    console.log('Connection success');
+})
